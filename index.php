eec73ea486c96cf3b70a7bfdf924d9cfb795dafd
Tanishq Sharma <tanishqsh@gmail.com>
2016-03-03 20:48:58 +0530

    Added files via upload
    Images folder to be extracted before uploading to the server. Contains logo, and SVGs of services.

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<!--
+- Need a web-site? Call Binary Caves today at 9819075165, 9167713562.
+-->
+<html>
+    <head>
+        <title>Binary Caves</title>
+        <link href="style.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="mobile.css" type="text/css" 
+    media="only screen and (max-width: 500px)">
+        <meta charset="UTF-8">
+        <meta name="Description" content="Binary Caves"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+        <meta name="keywords" content="Binary Caves, Marketing Agency, Digital Agency">
+        <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+        <div class="container">
+            <div class="header">
+                <div class="twoheads">
+                    <div class="headone">
+                        <div class="textzone">
+                            <a class="BinaryTitle">
+                                <span class="BinaryAlpha">Binary</span>
+                                <span class="BinaryAlpha">Caves</span>
+                        </a>
+                            <br>
+                            <a class="BinarySubTitle">
+                                <ul>
+                                    <li>Web Development</li>
+                                    <li>Mobile Application Development</li>
+                                    <li>Marketing Agency</li>
+                                    <li>Digital Agency</li>
+                                </ul>
+                            </a>
+                        </div>
+                        
+                    </div>
+                    <div class="headtwo">
+                        <a href="http://binarycaves.com/"><img class="imageContainer" src="http://s22.postimg.org/3k94khj35/Binary_Primary_Logo.png">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="three-divisions">
+                <div class="second-container">
+                    <p class="binaryfont"> 
+                        <img class="token-images" src="images/webdevelopment.svg"
+                             alt="Web Development">
+                    <p class="binaryfont"> Web Development</p>
+                    <button type="button" onclick="location.href='http://www.binarycaves.com'" 
+                                class="binary-primary-btn">Enquire</button>
+                    
+
+                </div>
+                <div class="third-container">
+                    <p class="binaryfont">
+                    <img class="token-images" src="images/marketing.svg"
+                             alt="Web Development">
+                    <p class="binaryfont"> Marketing Agency </p>
+                    <button type="button" onclick="location.href='http://www.binarycaves.com'" 
+                                class="binary-secondary-btn">Enquire</button>
+                </div>
+                <div class="fourth-container">
+                     <p class="binaryfont">
+                         <img class="token-images" src="images/digital.svg"
+                             alt="Web Development">
+                    <p class="binaryfont"> Digital Agency </p>
+                    <button type="button" onclick="location.href='http://www.binarycaves.com'" 
+                                class="binary-tertiary-btn">Enquire</button>
+                </div>
+                <div class="fifth-container">
+                     <p class="binaryfont">
+                         <img class="token-images" src="images/contact.svg"
+                             alt="Web Development">
+                    <p class="binaryfont">+91 9819075165</p>
+                    <p class="binaryfont">+91 9167713562</p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
